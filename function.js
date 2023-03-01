12eb3c4ab04293fb516b9e550498fdf4b9de0ad4
franferraras <126421435+franferraras@users.noreply.github.com>
2023-03-01 17:22:36 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,14 @@
+function Facebook (){
+    location.href = "https://www.facebook.com/"
+}
+
+function Twitter(){
+    location.href = "https://twitter.com/"
+}
+
+function LinkedIn(){
+    location.href="https://ar.linkedin.com/"
+}
+function displayDate() {
+    document.getElementById("demo").innerHTML = location.href = "http://letrasclasicas.filo.uba.ar/content/plan-de-estudios";
+}
